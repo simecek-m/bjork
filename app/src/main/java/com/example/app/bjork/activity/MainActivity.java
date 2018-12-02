d5151ac3f13fbc72177327c8a2982482de883b1b
Martin Simecek <simecek.m@seznam.cz>
2018-12-03 00:23:50 +0100

    hamburger menu - ikona, nastavení toolbaru NavigationView - boční navigace header view - šablona hlavičky (profilový obrázek, jméno, email) navigace menu - menu navigace - název, ikona

--- app/src/main/java/com/example/app/bjork/activity/MainActivity.java
+++ app/src/main/java/com/example/app/bjork/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.app.bjork.activity;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.app.bjork.R;
 import com.example.app.bjork.adapter.ScreenSlidePagerAdapter;
@@ -13,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +29,37 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        Drawable hamburgerIcon = getDrawable(R.drawable.ic_menu);
+        hamburgerIcon.setTint(getResources().getColor(R.color.white));
+        actionBar.setHomeAsUpIndicator(hamburgerIcon);
 
         viewPager = findViewById(R.id.viewPager);
         pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(pagerAdapter);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setCheckedItem(R.id.nav_products);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
