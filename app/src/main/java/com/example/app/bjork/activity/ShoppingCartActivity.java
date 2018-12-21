23f63c8241c62ef8b983a1564b7a69feea89a6ae
Martin Simecek <simecek.m@seznam.cz>
2018-12-21 18:37:00 +0100

    přidán progressBar při vytváření objednávky (smazání košíku)

--- app/src/main/java/com/example/app/bjork/activity/ShoppingCartActivity.java
+++ app/src/main/java/com/example/app/bjork/activity/ShoppingCartActivity.java
@@ -1,8 +1,9 @@
 package com.example.app.bjork.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.app.bjork.R;
@@ -35,6 +38,7 @@ import java.util.List;
 public class ShoppingCartActivity extends AppCompatActivity {
 
     private static final String TAG = "ShoppingCartActivity";
+    private static final int DELAY = 3000;
 
     private FirebaseAuth auth;
     private List<CartItem> list;
@@ -130,13 +134,32 @@ public class ShoppingCartActivity extends AppCompatActivity {
     public void createOrderBottomSheet() {
         orderBottomSheet = new BottomSheetDialog(this, R.style.BottomSheetDialog);
         orderBottomSheet.setContentView(R.layout.order_bottom_sheet);
-        Button confirmButton = orderBottomSheet.findViewById(R.id.confirmButton);
+        final Button confirmButton = orderBottomSheet.findViewById(R.id.confirmButton);
+        final LinearLayout progressLayout = orderBottomSheet.findViewById(R.id.progressLayout);
+        final ProgressBar progressBar = orderBottomSheet.findViewById(R.id.progressBar);
+        final TextView progressText = orderBottomSheet.findViewById(R.id.progressText);
+        final ConstraintLayout orderInfo = orderBottomSheet.findViewById(R.id.orderInfo);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BjorkAPI.newOrder();
-                orderBottomSheet.dismiss();
-                finish();
+                confirmButton.setVisibility(View.GONE);
+                progressLayout.setVisibility(View.VISIBLE);
+                orderInfo.setVisibility(View.GONE);
+                BjorkAPI.newOrder().addOnSuccessListener(new OnSuccessListener<HttpsCallableResult>() {
+                    @Override
+                    public void onSuccess(HttpsCallableResult httpsCallableResult) {
+                        progressBar.setVisibility(View.GONE);
+                        progressText.setText(getString(R.string.order_complete));
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                orderBottomSheet.dismiss();
+                                finish();
+                            }
+                        }, DELAY);
+                    }
+                });
             }
         });
         TextView  user = orderBottomSheet.findViewById(R.id.userText);
