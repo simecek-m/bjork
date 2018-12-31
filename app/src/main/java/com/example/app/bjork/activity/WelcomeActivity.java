b86185f583f84a0422af488c6556d3a5c9841e0d
Martin Simecek <simecek.m@seznam.cz>
2018-12-31 19:15:11 +0100

    redesign, nový splash screen

--- app/src/main/java/com/example/app/bjork/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/app/bjork/activity/WelcomeActivity.java
@@ -4,17 +4,44 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.app.bjork.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIMER = 3000;
+    private static final int SPLASH_TIMER = 4000;
+    private static final int ANIMATION_TIMER = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final ImageView icon = findViewById(R.id.icon);
+        final TextView appName = findViewById(R.id.app_name);
+        final TextView motto = findViewById(R.id.motto);
+
+        icon.animate()
+                .setDuration(ANIMATION_TIMER)
+                .translationY(0f)
+                .start();
+
+        appName.animate()
+                .setDuration(ANIMATION_TIMER)
+                .translationY(0f)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        motto.animate()
+                                .setDuration(ANIMATION_TIMER)
+                                .alpha(100)
+                                .start();
+                    }
+                })
+                .start();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
