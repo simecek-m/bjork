a77c3707faf38e0ebd182ec1f3c3a130f255bf05
Martin Simecek <simecek.m@seznam.cz>
2019-01-15 02:45:28 +0100

    - parse produktu z payloadu notifikace

--- app/src/main/java/com/example/app/bjork/service/BjorkFirebaseMessagingService.java
+++ app/src/main/java/com/example/app/bjork/service/BjorkFirebaseMessagingService.java
@@ -1,11 +1,17 @@
 package com.example.app.bjork.service;
 
 import com.example.app.bjork.api.BjorkAPI;
+import com.example.app.bjork.model.Product;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class BjorkFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static String NOTIFICATION_PAYLOAD_DATA = "product";
 
     @Override
     public void onNewToken(String token) {
@@ -23,6 +29,10 @@ public class BjorkFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        System.out.println("message received: " + remoteMessage.getData());
+        Gson gson = new Gson();
+        String stringifyProduct = remoteMessage.getData().get(NOTIFICATION_PAYLOAD_DATA);
+        JsonElement jsonElement = new JsonParser().parse(stringifyProduct);
+        Product product = gson.fromJson(jsonElement, Product.class);
+        System.out.println("product: " + product);
     }
 }
